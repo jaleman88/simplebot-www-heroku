e111232892033a3987561aa86b77d0166ceda9f1
Jesus <96878730+jaleman88@users.noreply.github.com>
2022-10-30 23:48:30 -0400

    Update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -4,7 +4,7 @@
 python3 -m simplebot init "$ADDR" "$PASSWORD"
 python3 -m simplebot -a "$ADDR" set_name "Jaleman-Web"
 python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/mode" "command"
-python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/max_size" "1073741824"  # 1GB
+python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/max_size" "1073741"  # 100mb
 python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/delay" $DELAY
 python3 -m simplebot -a "$ADDR" db -s "simplebot_translator/filter_enabled" "no"
 
